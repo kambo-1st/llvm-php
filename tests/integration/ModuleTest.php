850992c75fb244e00b160c6627230e94ae04efe5
kambo-1st <bohuslav@simek.si>
2019-01-20 16:00:28 +0100

    Dead simple code generation with c API

--- tests/integration/ModuleTest.php
+++ tests/integration/ModuleTest.php
@@ -1,33 +1,58 @@
 <?php
 
-namespace Kambo\Tests\Yasna\Unit;
+namespace Kambo\Tests\LLVM\Intergration;
 
 use PHPUnit\Framework\TestCase;
-use Kambo\LLVM\Module;
-use Kambo\LLVM\FunctionType;
-use Kambo\LLVM\Type;
+use Kambo\LLVM\LLVM;
 
 /**
- * Unit tests for class Kambo\LLVM\Module
+ * Unit tests for class Kambo\LLVM\LLVM
  *
  * @author Bohuslav Simek <bohuslav@simek.si>
  */
 class ModuleTest extends TestCase
 {
     /**
-     * Tests load bytecode
+     * Tests printing module into the string
      *
      * @return void
      */
     public function testPrintModuleToString(): void
     {
-        $module = new Module('my_module');
+        $llvm   = new LLVM();
+        $module = $llvm->LLVMModuleCreateWithName("my_module");
+
         $paramTypes = [
-            Type::getInt32Ty(), Type::getInt32Ty(),
+            $llvm->LLVMInt32Type(),
+            $llvm->LLVMInt32Type()
         ];
-        $functionType = FunctionType::get(Type::getInt32Ty(), $paramTypes,0);
 
+        $retType  = $llvm->LLVMFunctionType($llvm->LLVMInt32Type(), $paramTypes, 2, 0);
 
-        $this->assertEquals($expectedSections, $bytecode->getSections());
+        $sum   = $llvm->LLVMAddFunction($module, 'sum', $retType);
+        $entry = $llvm->LLVMAppendBasicBlock($sum, 'entry');
+
+        $builder = $llvm->LLVMCreateBuilder();
+        $llvm->LLVMPositionBuilderAtEnd($builder, $entry);
+
+        $tmp = $llvm->LLVMBuildAdd($builder, $llvm->LLVMGetParam($sum, 0), $llvm->LLVMGetParam($sum, 1), 'tmp');
+
+        $llvm->LLVMBuildRet($builder, $tmp);
+
+        $moduleString = $llvm->LLVMPrintModuleToString($module);
+
+        $expectedModuleString = <<<EOT
+; ModuleID = 'my_module'
+source_filename = "my_module"
+
+define i32 @sum(i32, i32) {
+entry:
+  %tmp = add i32 %0, %1
+  ret i32 %tmp
+}
+
+EOT;
+
+        $this->assertEquals($expectedModuleString, $moduleString);
     }
 }
